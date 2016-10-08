50cbc0fb95db5a348b11e5fb5f17d46a1731cef4
Mark Loeser <mark.loeser@gmail.com>
2016-10-08 12:56:25 -0400

    adding more responsiveness

--- javascript.js
+++ javascript.js
@@ -6,29 +6,15 @@
 when we've scrolled past it */
 var mainbottom = $('#about').offset().top + $('#about').height();
 
-
 $(window).on('scroll',function(){
 
     // we round here to reduce a little workload
     var stop = Math.round($(window).scrollTop());
 
     if (stop > mainbottom) {
-    	$('.navbar').addClass('navbar-past-main');
-        $('.navbar-right').addClass('past-main');
         $('.navbar-brand').addClass('brand-past-main');
     } else {
-    	$('.navbar').removeClass('navbar-past-main');
-        $('.navbar-right').removeClass('past-main');
         $('.navbar-brand').removeClass('brand-past-main');
     }
 
 });
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
